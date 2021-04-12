0b13063896c0c9f8ef5a7a96875756f93854541b
truongdinhthien <truongdinhthien260599@gmail.com>
2021-04-12 21:38:13 +0700

    as

--- brute-force/index.js
+++ brute-force/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 3030;
-const htmlPath = path.join(__dirname, 'public');
-app.use(express.static(htmlPath));
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'index.html');
+});
+
 app.listen(port, () => {
   console.log(`App is listening on ${port}`);
 });
